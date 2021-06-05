6c98ab661d4bd7f712a58b2d160a9fd2065dd481
pmh-projects <77459300+pmh-projects@users.noreply.github.com>
2021-06-05 19:24:06 +0200

    Update testZapisNotatka.py

--- testZapisNotatka.py
+++ testZapisNotatka.py
@@ -1,11 +1,11 @@
 import sys
-import io
-import pytest
-import unittest
 from nose.tools import *
 from unittest import mock
 from unittest.mock import patch
 from mock import patch
+import io
+import pytest
+import unittest
 
 def save_to_file_title():
         print("Wpisz nazwe tytulu lub 0, aby wyjsc")
@@ -57,67 +57,60 @@ def save_to_file_content(tytul):
 
             print(e)
 
+
 class Test(unittest.TestCase):
 
     # Sprawdza czy w funkcji podano nazwe (Rozne od 0). Test na zwykla nazwe bez liczby
     def testTitleIfNotZero(self):
 
         mock.builtins.input = lambda _: "Testowy tytul"
-        assert_equal(save_to_file_title(), "Testowy tytul")
-
+        self.assertEqual(save_to_file_title(), "Testowy tytul")
 
     # Test zapisu tytulu w postaci liczby
     def testTitleIfNumber(self):
 
         mock.builtins.input = lambda _: "123"
-        assert_equal(save_to_file_title(), "123")
-
+        self.assertEqual(save_to_file_title(), "123")
 
     # Test funkcji w wypadku podania braku tytulu
     def testTitleIfEmpty(self):
 
         mock.builtins.input = lambda _: ""
-        assert_equal(save_to_file_title(), "Brak tytulu")
-
+        self.assertEqual(save_to_file_title(), "Brak tytulu")
 
     # Test zbyt dlugiego tytulu
     def testTitleIfTooLong(self):
 
         mock.builtins.input = lambda _: "Zbyt Dlugi Tytul do testow maksymalna liczba znaków to dwadzieścia pięć"
-        assert_equal(save_to_file_title(), 5)
-
+        self.assertEqual(save_to_file_title(), 5)
 
     # Test wpisu do notatki z TESTowym tytulem zadeklarowanym w tesci
     def testContentPositive(self):
 
         tytul="TEST"
         mock.builtins.input = lambda _: "Testowy wpis do notatki"
-        assert_equal(save_to_file_content(tytul), "Plik został zapisany.")
-
+        self.assertEqual(save_to_file_content(tytul), "Plik został zapisany.")
 
     # Test zapisu tresci w postaci samych liczb
     def testContentNumbers(self):
 
         tytul = "TEST4"
         mock.builtins.input = lambda _: "123456"
-        assert_equal(save_to_file_content(tytul), "Plik został zapisany.")
-
+        self.assertEqual(save_to_file_content(tytul), "Plik został zapisany.")
 
     # Test wpisu do notatki z TESTowym tytulem zadeklarowanym w tesci
-    def testContentNameShort(self):
+    def testContentNegativeShort(self):
 
         tytul="TEST2"
         mock.builtins.input = lambda _: "test"
-        assert_equal(save_to_file_content(tytul), "Notatka zbyt krotka")
+        self.assertEqual(save_to_file_content(tytul), "Notatka zbyt krotka")
 
     # Test z brakiem wpisu do notatki z TESTowym tytulem zadeklarowanym w tesci
-    def testContentEmpty(self):
+    def testContentNegativeEmpty(self):
 
         tytul="TEST3"
         mock.builtins.input = lambda _: ""
-        assert_equal(save_to_file_content(tytul), "Notatka zbyt krotka")
-
-
+        self.assertEqual(save_to_file_content(tytul), "Notatka zbyt krotka")
 
 if __name__ == "__main__":
 
