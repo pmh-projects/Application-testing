a49174fa24801c683149e55b78ab4f2e33240e68
pmh-projects <77459300+pmh-projects@users.noreply.github.com>
2021-06-05 10:36:05 +0200

    Update testOpen.py

--- testOpen.py
+++ testOpen.py
@@ -1,42 +1,7 @@
-import random
-# Moduł przeglądarki internetowej zapewnia interfejs
-# wysokiego poziomu, który umożliwia wyświetlanie użytkownikom dokumentów internetowych.
-# https://docs.python.org/3/library/webbrowser.html
-import webbrowser
-# biblioteka konwersji tekstu na mowę w języku Python.
-# https://pypi.org/project/pyttsx3/
-import pyttsx3
-# Moduł sys w Pythonie zapewnia różne funkcje i zmienne,
-# które są używane do manipulowania różnymi częściami środowiska wykonawczego Pythona.
-# https://docs.python.org/3/library/sys.html
-import sys
-# wieloplatformowy moduł do automatyzacji GUI w języku Python dla ludzi.
-# Służy do programowego sterowania myszą i klawiaturą.
-# https://pypi.org/project/PyAutoGUI/
-import pyautogui
-# Googlesearch to biblioteka Pythona do wyszukiwania w Google.
-# https://pypi.org/project/googlesearch-python/
-from googlesearch import search
-# Wrapper do OpenWeatherMap https://pypi.org/project/pyowm/ (
-import pyowm
-from pyowm.utils.config import get_default_config
-# Pakiet tkinter („interfejs Tk”) jest standardowym interfejsem
-# Pythona do zestawu narzędzi Tk GUI.
-# https://docs.python.org/3/library/tkinter.html
-import tkinter as tk
-import tkinter.font as tkFont
-from tkinter import *
-# Pillow to fork PIL. PIL to biblioteka obrazowania języka Python autorstwa Fredrika Lundha i współautorów.
-# https://pypi.org/project/Pillow/
-from PIL import ImageTk, Image
-# Ten moduł zapewnia różne funkcje związane z czasem.
-# https://docs.python.org/3/library/time.html
-import time
 from nose.tools import *
 from unittest import mock
 from unittest.mock import patch
 from mock import patch
-import io
 import pytest
 import unittest
 
